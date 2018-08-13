df068cbbfb51a2b3f904f8850f2f5086b7c7facc
Gilberto Souza <gilberto.souza@fusiondms.com.br>
2018-08-13 14:22:49 -0300

    insert panel and cards

--- app/src/containers/Home.js
+++ app/src/containers/Home.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
+import Panel from './../components/Panel'
+import Card from './../components/Card'
 import './Home.scss'
+
 
 class Home extends Component {
     constructor(props){
@@ -8,27 +11,10 @@ class Home extends Component {
 
     render() {
         return (
-            <div>
-            <nav id="navbar-center" className="navbar navbar-default navbar-fixed-top">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        <a href="" className="navbar-brand">
-                            <img src="/images/tilst.png" className="img-responsive" alt="Tilst" />
-                        </a>
-                    </div>
-                    <div className="navbar-collapse collapse" id="navbar">
-                        <ul className="nav navbar-nav">
+            <div>            
+                <Panel>
 
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-            <h1> Kanban Board React.js Redux </h1>
+                </Panel>
             </div>
         )
     }
